9ca928a465a18c0e308d4277aab25ebe60736523
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2025-05-23 14:50:21 -0300

    Refactor: Update comment for clarity in changePassword function

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -11,7 +11,7 @@ export const register = async (data: FormData) => {
   return res.data;
 };
 
-// Token
+// Token tresy
 export const changePassword = async (data: {
   old_password: string;
   new_password: string;
